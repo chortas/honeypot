474f087a7950e3236fe780132aa6d311db044979
chortas <chortas@fi.uba.ar>
2021-11-14 11:36:29 -0300

    Update selenium version

--- html_honeypot/scrapper/main.py
+++ html_honeypot/scrapper/main.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("http://0.0.0.0:5000/form")
 
-inputs = driver.find_elements_by_xpath(f"/html/body/form/input")
+inputs = driver.find_elements(By.XPATH, "/html/body/form/input")
 
 for input in inputs:
     input.send_keys("bot_input")
 
-form = driver.find_element_by_xpath(f"/html/body/form[1]")
+form = driver.find_element(By.XPATH, "/html/body/form[1]")
 
 form.submit()
 
