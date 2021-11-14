9abd1606c5685e0efc4a6865b3120f8f89779103
chortas <chortas@fi.uba.ar>
2021-11-14 11:15:22 -0300

    Reject malicious ips

--- html_honeypot/app/main.py
+++ html_honeypot/app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask.helpers import make_response
 
 app = Flask(__name__)
+
+ips = set()
 
 @app.route('/form')
 def form():
@@ -9,7 +12,13 @@ def form():
 @app.route('/validate', methods=["POST"])
 def validate():
     data = request.form
-    if data.get('fake_name') or data.get('fake_email'):
+    ip = request.remote_addr
+    if ip in ips or data.get('fake_name') or data.get('fake_email'):
+        if ip not in ips:
+            print("Malicious ip not registered")
+            ips.add(ip)
+        else:
+            print("Already registered malicious ip")
         return redirect(url_for('bot'))
     return redirect(url_for('person'))
 
@@ -19,8 +28,7 @@ def person():
 
 @app.route('/bot')
 def bot():
-    return render_template('error_bot.html')
-
+    return make_response(render_template('error_bot.html'), 404)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
