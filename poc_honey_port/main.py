a6a105297e39da26150b71e428f91df4531c0899
Franco Martin Di Maria <franki.dimaria@gmail.com>
2021-11-14 18:50:07 -0300

    add: logs to server

--- poc_honey_port/main.py
+++ poc_honey_port/main.py
@@ -1,7 +1,12 @@
 import args_parser
 import server as svr
+import sys
     
 if __name__ == '__main__':
-    args = args_parser.parse()
-    server = svr.Server(args.address, args.port)
-    server.accept_connections()
+    try:
+        args = args_parser.parse()
+        server = svr.Server(args.address, args.port)
+        server.accept_connections()
+    except Exception as e:
+        print(str(e))
+        sys.exit(1)
--- poc_honey_port/server.py
+++ poc_honey_port/server.py
@@ -1,33 +1,29 @@
-import sys
 from socket import socket, AF_INET, SOCK_STREAM
-import traceback
-
-DEFAULT_ADDRESS = "127.0.0.1" # Localhost
 
 class Server:
     def __init__(self, address, port):
-        try:
-            self.acceptor = socket(AF_INET,SOCK_STREAM)
-            self.acceptor.bind((address, port))
-            self.acceptor.listen()
-        except Exception:
-            traceback.print_exc()
-            sys.exit()
+        self.acceptor = socket(AF_INET,SOCK_STREAM)
+        self.acceptor.bind((address, port))
+        self.acceptor.listen()
 
     def destroy(self):
         self.acceptor.close()
+
+    def handle_connection(self, _conn, _addr):
+        pass
 
     def accept_connections(self):
         try:
             while True:
                 conn,addr = self.acceptor.accept()
-                print('honeypot has been visited by ' + addr[0])
+                print('[Honeypot] Accepted connection from ' + addr[0])
+                self.handle_connection(conn, addr)
                 conn.close()
-                # while True:
-                #     data=conn.recv(1024)
-                #     if data == b'\r\n':
-                #         ski.close()
-                #         sys.exit()
-        except Exception:
-            traceback.print_exc()
+                print('[Honeypot] Closed connection from ' + addr[0])
+        except KeyboardInterrupt:
+            print("[Honeypot] Shutting down.")
+            pass
+        except Exception as e:
+            raise e
+        finally:
             self.destroy()
