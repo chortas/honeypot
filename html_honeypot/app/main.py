8bc17cca1e8c74eeb91d0d108e4f68eee39d5d02
Oli <olifer97@gmail.com>
2021-12-08 12:30:31 -0300

    add cookies to secure access

--- html_honeypot/app/main.py
+++ html_honeypot/app/main.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask.helpers import make_response
 
 app = Flask(__name__)
 
+ACCEPTED_COOKIE = "person-cookie"
+
+BOT_COOKIE = "bot-cookie"
+
 ips = set()
+
+def check_if_not_logged(cookie):
+    if not cookie:
+        return make_response(redirect(url_for('form')))
+
+def check_if_person(cookie):
+    if cookie and cookie == ACCEPTED_COOKIE:
+        return make_response(redirect(url_for('person')))
+
+def check_if_bot(cookie):
+    if cookie and cookie != ACCEPTED_COOKIE:
+        return make_response(redirect(url_for('person')))
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    cookie = request.cookies.get('secret')
+    resp = check_if_person(cookie) or check_if_bot(cookie)
+    return resp or render_template('form.html')
 
 @app.route('/validate', methods=["POST"])
 def validate():
@@ -19,16 +37,24 @@ def validate():
             ips.add(ip)
         else:
             print("Already registered malicious ip")
-        return redirect(url_for('bot'))
-    return redirect(url_for('person'))
+        resp = make_response(redirect(url_for('bot')))
+        resp.set_cookie('secret', BOT_COOKIE)
+        return resp
+    resp = make_response(redirect(url_for('person')))
+    resp.set_cookie('secret', ACCEPTED_COOKIE)
+    return resp
 
 @app.route('/person')
 def person():
-    return render_template('ok_person.html')
+    cookie = request.cookies.get('secret')
+    resp = check_if_not_logged(cookie) or check_if_bot(cookie)
+    return resp or render_template('ok_person.html')
 
 @app.route('/bot')
 def bot():
-    return make_response(render_template('error_bot.html'), 404)
+    cookie = request.cookies.get('secret')
+    resp = check_if_not_logged(cookie)
+    return resp or make_response(render_template('error_bot.html'), 404)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
